df82e5f6d779677cbfb444575416901bd49272f7
Gaurav Asode <gauravasode5@gmail.com>
2022-11-30 10:22:02 +0530

    prime_and_pelindrome_ok

--- prime_and_pelindrome.js
+++ prime_and_pelindrome.js
@@ -6,3 +6,17 @@ if(num%i==0){
 flag=true;
 }
 }
+if(flag==true){
+console.log("Prime");
+}else{
+console.log("Not Prime");
+}
+
+
+
+let str="naman";
+let bag="";
+for(let j=str.length-1;j>=0; j--){
+bag+=str[j];
+}
+console.log(bag)
